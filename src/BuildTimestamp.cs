55c3b3c06945eae00fef22b05ad3bc2f9ac3504e
pubpub-zz <4083478+pubpub-zz@users.noreply.github.com>
2021-05-30 14:44:56 +0200

    New Release : 1.6RC1
    new Release and doc update

--- src/BuildTimestamp.cs
+++ src/BuildTimestamp.cs
@@ -1,1 +1,1 @@
-namespace gInk { public static class Build { public static string Timestamp = "24/03/2021 13:19:34.68".Substring(0,16);}} 
+namespace gInk { public static class Build { public static string Timestamp = "30/05/2021 13:02:59.51".Substring(0,16);}} 
